4d7a5fc7040fea075d95ce534b875dc5ce0d34c9
AxisFray <113852846+AxisFray@users.noreply.github.com>
2023-12-05 15:11:43 +0100

    Add files via upload

--- plik.py
+++ plik.py
@@ -19,48 +19,7 @@ def Pr1():
         wariancja += (srednia-i)**2
     wariancja =wariancja/len(x)
     print(f"Suma to:{suma},Srednia to:{srednia},najwieksza liczba to:{maxx}, a najmniejsza:{minn}, wariancja to:{wariancja}")
-def Pr3():
-    aa = open("in.txt","r")
-    text = aa.read()
-    ac = open("dict.txt","a")
-    
-    ab = open("dict.txt","r")
-    text2 = ab.read()
-    
-    for i in text:
-        if i=="," or i=="." or i==";" or i=='"':
-            i="."
-    
-    text.lower()
-    text.strip(".")
-    text.split(" ")
-    text2.split(" ")
-    print(text)
-    print(text2)
-    text3 = ""
-    '''
-    for i in text:
-        if i not in text2 :
-            ac.write(i+" ")
-            print(i)
-    
-    no = 0
-    for i in text:
-        if no==1:
-            text2.append(text[i-1])
-        for j in text2:
-            if i==j:
-                no =1
-                '''
-    print(text2)
-    print(text)
 
-    text2 += text3
-    ac.write(text2)
-    aa.close()
-    ab.close()
-    ac.close()
-            
         
              
 
@@ -84,5 +43,76 @@ def Pr2():
         else:
             odszyfrowany += chr(ord(zaszyfrowany[d]) - klucz)
     print("Odszyfrowany tekst:",odszyfrowany)
-Pr3()
+
+
+
+
+def Pr3():
+    aa = open("in.txt","r")
+    text = aa.read()
+    #plik z tekstem text
+    ac = open("dict.txt","a")
+    
+    #plik slownik text2
+    ab = open("dict.txt","r")
+    text2 = ab.read()
+    zapisz = open("dict.txt","w")
+    
+    for i in text:
+        if i=="," or i=="." or i==";" or i=='"':
+            i.replace(".")
+    
+    text.lower()
+    text.strip(".")
+    text.split(" ")
+    
+    text2.split(" ")
+    print(text)
+    print(text2)
+    
+   
+    
+    powt = 0
+    for i in text2:
+        for j in text:
+            if i==j:
+                powt+=1
+        if powt==0:
+            text2.append(j)
+        powt=0
+
+
+
+
+    
+    print(text2)
+    print(text)
+    text3=""
+    for i in text2:
+        text3+= " "+i
+    zapisz.write(text3)
+    zapisz.close()
+    aa.close()
+    ab.close()
+    ac.close()
+
+
+def zad3():
+    with open('in_3.txt', 'r') as f:
+        slowa = f.read().split()
+        slownik = set()
+        try:
+            with open("dict.txt",'r') as f:
+                slownik = set(line.strip() for line in f)
+
+        except FileNotFoundError:
+            with open('dict.txt', 'w') as f:
+              pass
+        slownik.update(word.lower() for word in slowa)
+
+        with open('dict.txt','w') as f:
+            for slowo in sorted(slownik):
+                f.write(slowo+'\n')
+
+zad3()
  
